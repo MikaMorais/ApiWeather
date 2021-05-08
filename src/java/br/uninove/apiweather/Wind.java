820a6725ee5e97795b363a0c54f5b45058093890
Moises Morais <mika.devs100@gmail.com>
2021-05-07 22:17:57 -0300

    app api weather

---
+++ src/java/br/uninove/apiweather/Wind.java
@@ -0,0 +1,34 @@
+
+package br.uninove.apiweather;
+
+import javax.annotation.Generated;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+@Generated("jsonschema2pojo")
+public class Wind {
+
+    @SerializedName("speed")
+    @Expose
+    private Double speed;
+    @SerializedName("deg")
+    @Expose
+    private Integer deg;
+
+    public Double getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(Double speed) {
+        this.speed = speed;
+    }
+
+    public Integer getDeg() {
+        return deg;
+    }
+
+    public void setDeg(Integer deg) {
+        this.deg = deg;
+    }
+
+}
